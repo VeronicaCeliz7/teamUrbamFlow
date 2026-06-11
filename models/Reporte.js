7c4a31aab4d63ee846bc8766f98f934e44e2c76a
Gabriel Fernandez <hridya.cloud@gmail.com>
2026-06-10 21:55:40 -0300

    feat(ia): clasificacion automatica, prioridad inteligente y duplicados geoespaciales

--- models/Reporte.js
+++ models/Reporte.js
@@ -83,15 +83,55 @@ const ReporteSchema = new mongoose.Schema({
 
   etiquetas: [{ type: String }],
 
-  ai_summary: { type: String, default: null },
-  ai_priority_score: { type: Number, default: null },
+  ai_summary: {
+  type: String,
+  default: ''
+},
 
-  posible_duplicado: { type: Boolean, default: false },
-  reporte_duplicado_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Reporte',
-    default: null
-  },
+proveedor_ia: {
+  type: String,
+  default: ''
+},
+
+modelo_ia: {
+  type: String,
+  default: ''
+},
+
+ai_priority_score: {
+  type: Number,
+  default: 0
+},
+
+posible_duplicado: {
+  type: Boolean,
+  default: false
+},
+
+reporte_duplicado_id: {
+  type: String,
+  default: null
+},
+
+duplicado_score: {
+  type: Number,
+  default: 0
+},
+
+duplicado_distancia_metros: {
+  type: Number,
+  default: null
+},
+
+vectorizado: {
+  type: Boolean,
+  default: false
+},
+
+vector_modelo: {
+  type: String,
+  default: null
+},
 
   // Estado actual del reporte
   estado: {
--- routes/iaRoutes.js
+++ routes/iaRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    clasificarIncidente
+  clasificarIncidente,
+  detectarDuplicado
 } = require('../controllers/iaController');
 
 router.post('/clasificar', clasificarIncidente);
+router.post('/duplicado', detectarDuplicado);
 
 module.exports = router;
